c81ef320b0f98c2114c5a25c1f690cbb850e00ce
Flores-AM <agustinmatias62@gmail.com>
2022-11-02 21:15:38 -0300

    validacion form

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import ubicacion from "../img/ubicacion.avif";
 import email from "../img/email.avif";
 import viber from "../img/viber.avif";
@@ -7,6 +8,7 @@ import phone from "../img/phone.avif";
 import linkedin from "../img/linkedin.avif";
 
 const Contacto = () => {
+  const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
   return (
     <div id="contacto">
       <section className="caja-contacto">
@@ -65,39 +67,145 @@ const Contacto = () => {
               </li>
             </ul>
           </div>
-          <div
-            className="contactForm"
-            data-aos="zoom-in"
-            data-aos-duration="1500"
-            data-aos-delay="300"
+          <Formik
+            initialValues={{
+              nombre: "",
+              apellido: "",
+              correo: "",
+              celular: "",
+              mensaje: "",
+            }}
+            validate={(valores) => {
+              let errores = {};
+
+              // Validacion nombre
+              if (!valores.nombre) {
+                errores.nombre = "Por favor ingrese su nombre";
+              } else if (!/^[a-zA-ZÀ-ÿ\s]{3,40}$/.test(valores.nombre)) {
+                errores.nombre =
+                  "El nombre solo puede contener letras y espacios";
+              }
+
+              // Validacion apellido
+              if (!valores.apellido) {
+                errores.apellido = "Por favor ingrese su apellido";
+              } else if (!/^[a-zA-ZÀ-ÿ\s]{3,40}$/.test(valores.apellido)) {
+                errores.apellido =
+                  "El apellido solo puede contener letras y espacios";
+              }
+
+              // Validacion email
+              if (!valores.correo) {
+                errores.correo = "Por favor ingrese su correo";
+              } else if (
+                !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+                  valores.correo
+                )
+              ) {
+                errores.correo =
+                  "El correo solo puede contener letras, numeros, puntos y guiones";
+              }
+
+              // Validacion celular
+              if (!valores.celular) {
+                errores.celular = "Por favor ingrese su celular";
+              } else if (!/^[0-9]{10,14}$/.test(valores.celular)) {
+                errores.celular = "El celular solo puede contener numeros";
+              }
+
+              // Validacion mensaje
+              if (!valores.mensaje) {
+                errores.mensaje = "Por favor deje su mensaje";
+              }
+              return errores;
+            }}
+            onSubmit={(valores, { resetForm }) => {
+              resetForm();
+              cambiarFormularioEnviado(true);
+              setTimeout(() => cambiarFormularioEnviado(false), 10000);
+            }}
           >
-            <h2>Enviar Mensaje</h2>
-            <div className="formBox">
-              <div className="inputBox w50">
-                <input type="text" required />
-                <span>Nombre</span>
-              </div>
-              <div className="inputBox w50">
-                <input type="text" required />
-                <span>Apellido</span>
-              </div>
-              <div className="inputBox w50">
-                <input type="email" required />
-                <span>Email</span>
-              </div>
-              <div className="inputBox w50">
-                <input type="text" required />
-                <span>Celular</span>
-              </div>
-              <div className="inputBox w100">
-                <textarea required></textarea>
-                <span>Deje su Mensaje</span>
-              </div>
-              <div className="inputBox w100">
-                <input type="submit" value="Enviar" />
-              </div>
-            </div>
-          </div>
+            {({ errors }) => (
+              <Form
+                className="contactForm"
+                data-aos="zoom-in"
+                data-aos-duration="1000"
+                data-aos-delay="200"
+              >
+                <h2>Enviar Mensaje</h2>
+                <div className="formBox">
+                  <div className="inputBox w50">
+                    <Field type="text" id="nombre" name="nombre" required />
+                    <span htmlFor="nombre">Nombre</span>
+                    <ErrorMessage
+                      name="nombre"
+                      component={() => (
+                        <div className="error">{errors.nombre}</div>
+                      )}
+                    />
+                  </div>
+                  <div className="inputBox w50">
+                    <Field type="text" id="apellido" name="apellido" required />
+                    <span htmlFor="correo">Apellido</span>
+                    <ErrorMessage
+                      name="apellido"
+                      component={() => (
+                        <div className="error">{errors.apellido}</div>
+                      )}
+                    />
+                  </div>
+                  <div className="inputBox w50">
+                    <Field type="email" id="correo" name="correo" required />
+                    <span htmlFor="correo">Email</span>
+                    <ErrorMessage
+                      name="correo"
+                      component={() => (
+                        <div className="error">{errors.correo}</div>
+                      )}
+                    />
+                  </div>
+                  <div className="inputBox w50">
+                    <Field type="text" id="celular" name="celular" required />
+                    <span>Celular</span>
+                    <ErrorMessage
+                      name="celular"
+                      component={() => (
+                        <div className="error">{errors.celular}</div>
+                      )}
+                    />
+                  </div>
+                  <div className="inputBox w100">
+                    <Field
+                      as="textarea"
+                      id="mensaje"
+                      name="mensaje"
+                      required
+                    ></Field>
+                    <span>Deje su Mensaje</span>
+                    <ErrorMessage
+                      name="mensaje"
+                      component={() => (
+                        <div className="error">{errors.mensaje}</div>
+                      )}
+                    />
+                  </div>
+                  <div className="inputBox w100 enviar">
+                    <input type="submit" value="Enviar" />
+                    {formularioEnviado && (
+                      <p
+                        className="exito"
+                        data-aos="zoom-in"
+                        data-aos-duration="800"
+                        data-aos-delay="200"
+                      >
+                        Formulario enviado con éxito!
+                      </p>
+                    )}
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </section>
     </div>
